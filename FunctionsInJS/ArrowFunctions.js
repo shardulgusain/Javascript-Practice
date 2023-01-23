7a0562612443c8cb7148b86299a41d5613f082bc
Shardul Gusain <shardulsgusain1@gmail.com>
2023-01-23 10:37:49 +0530

    arrow functions

---
+++ FunctionsInJS/ArrowFunctions.js
@@ -0,0 +1,15 @@
+// function greet() {
+//     console.log("hello shardul");
+// }
+
+let greet = ()=>{
+    console.log("Hello shardul");
+}
+greet();
+
+
+let sum = (x,y)=>{
+    return x+y;
+}
+
+console.log(sum(5,10));
